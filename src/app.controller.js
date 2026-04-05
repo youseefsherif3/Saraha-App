8b022cebaa5561956da7c0e86e881045b767c2a2
youseefsherif3 <youseefsherif89@gmail.com>
2026-04-05 16:16:30 +0200

    Final Vercel Error

--- src/app.controller.js
+++ src/app.controller.js
@@ -14,7 +14,7 @@ const port = PORT;
 const bootstrap = async () => {
   const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    limit: 3,
+    limit: 100,
     legacyHeaders: false,
     message: "Too many requests from this IP, please try again after 15 minutes",
   });
